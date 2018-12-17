4d7fb716f6ff521bc2c85bf655f0fbd2a9aa1054
parveshtandonin <parvesh.tandon@infogain.com>
2018-12-17 16:24:35 +0530

    POC:
    Design basic grid.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,217 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="flex--cont--def app--container">
+
+        <div className="flex--cont--def app-header--cont">
+          <div className="app-header--child app--header--child--a">
+            COunt
+          </div>
+          <div className="app-header--child app--header--child--b">
+            Add
+          </div>
+          <div className="app-header--child app--header--child--c">
+            filter icon
+          </div>
+          <div className="app-header--child app--header--child--d">
+            <input type='text'
+              name='search--filter'
+              id='search--filter'
+              placeholder='Search here' />
+          </div>
+        </div>
+        <div className="flex--cont--def grid--columns--container">
+          <div className="grid--columns--container--child--def grid--columns--container--child--a">
+            Name
+          </div>
+          <div className="grid--columns--container--child--def grid--columns--container--child--a">
+            Order Date
+          </div>
+          <div className="grid--columns--container--child--def grid--columns--container--child--a">
+            Unit
+          </div>
+          <div className="grid--columns--container--child--def grid--columns--container--child--a">
+            discount
+          </div>
+          <div className="grid--columns--container--child--def grid--columns--container--child--a">
+            in stock
+          </div>
+          <div className="grid--columns--container--child--def grid--columns--container--child--a">
+            edit-delete
+          </div>
+        </div>
+        <div className="flex--cont--def grid--row--container">
+          <div className="grid--row--child--container">
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </div>
+          <div className="grid--row--child--container">
+            <input type="date" name="bday" />
+          </div>
+          <div className="grid--row--child--container">
+            1
+          </div>
+          <div className="grid--row--child--container">
+            <input
+              name="isGoing"
+              type="checkbox"
+            />
+          </div>
+          <div className="grid--row--child--container">
+            yes
+          </div>
+          <div className="grid--row--child--container action--container">
+            edit-delete
+          </div>
+        </div>
+        <div className="flex--cont--def grid--row--container">
+          <div className="grid--row--child--container">
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </div>
+          <div className="grid--row--child--container">
+            <input type="date" name="bday" />
+          </div>
+          <div className="grid--row--child--container">
+            1
+          </div>
+          <div className="grid--row--child--container">
+            <input
+              name="isGoing"
+              type="checkbox"
+            />
+          </div>
+          <div className="grid--row--child--container">
+            yes
+          </div>
+          <div className="grid--row--child--container action--container">
+            edit-delete
+          </div>
+        </div>
+        <div className="flex--cont--def grid--row--container">
+          <div className="grid--row--child--container">
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </div>
+          <div className="grid--row--child--container">
+            <input type="date" name="bday" />
+          </div>
+          <div className="grid--row--child--container">
+            1
+          </div>
+          <div className="grid--row--child--container">
+            <input
+              name="isGoing"
+              type="checkbox"
+            />
+          </div>
+          <div className="grid--row--child--container">
+            yes
+          </div>
+          <div className="grid--row--child--container action--container">
+            edit-delete
+          </div>
+        </div>
+        <div className="flex--cont--def grid--row--container">
+          <div className="grid--row--child--container">
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </div>
+          <div className="grid--row--child--container">
+            <input type="date" name="bday" />
+          </div>
+          <div className="grid--row--child--container">
+            1
+          </div>
+          <div className="grid--row--child--container">
+            <input
+              name="isGoing"
+              type="checkbox"
+            />
+          </div>
+          <div className="grid--row--child--container">
+            yes
+          </div>
+          <div className="grid--row--child--container action--container">
+            edit-delete
+          </div>
+        </div>
+        <div className="flex--cont--def grid--row--container">
+          <div className="grid--row--child--container">
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </div>
+          <div className="grid--row--child--container">
+            <input type="date" name="bday" />
+          </div>
+          <div className="grid--row--child--container">
+            1
+          </div>
+          <div className="grid--row--child--container">
+            <input
+              name="isGoing"
+              type="checkbox"
+            />
+          </div>
+          <div className="grid--row--child--container">
+            yes
+          </div>
+          <div className="grid--row--child--container action--container">
+            edit-delete
+          </div>
+        </div>
+        <div className="flex--cont--def grid--row--container">
+          <div className="grid--row--child--container">
+            <select>
+              <option value="grapefruit">Grapefruit</option>
+              <option value="lime">Lime</option>
+              <option selected value="coconut">Coconut</option>
+              <option value="mango">Mango</option>
+            </select>
+          </div>
+          <div className="grid--row--child--container">
+            <input type="date" name="bday" />
+          </div>
+          <div className="grid--row--child--container">
+            1
+          </div>
+          <div className="grid--row--child--container">
+            <input
+              name="isGoing"
+              type="checkbox"
+            />
+          </div>
+          <div className="grid--row--child--container">
+            yes
+          </div>
+          <div className="grid--row--child--container action--container">
+            edit-delete
+          </div>
+        </div>
       </div>
     );
   }
