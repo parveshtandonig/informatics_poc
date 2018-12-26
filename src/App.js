b2af207e5a40a52f6f2937eff40ffdb01871316e
Aditya Kumar Rauniyar <Aditya.Rauniyar@igglobal.com>
2018-12-26 16:59:19 +0530

    implemented internationalization

--- src/App.js
+++ src/App.js
@@ -8,14 +8,26 @@ import Sidebar from './components/sidebar/sidebar'
 import Footer from './components/footer/footer'
 
 import AppRouter from './appRouter'
+import { translate, Trans } from "react-i18next";
 
 class App extends Component {
 
   render() {
-
+    const { t, i18n } = this.props; 
+    const changeLanguage = lng => {
+      i18n.changeLanguage(lng);
+    };
     return (
+      
       <div className="flex--cont--def app--container">
+       
+       <h2>{t("Welcome to React")}</h2>
+       <p>
+        <span onClick={() => changeLanguage("de")}>de</span>..../.....
+        <span onClick={() => changeLanguage("en")}>en</span>  
+        </p> 
         <Header />
+        
         <div className="flex--cont--def app-middle--container">
           <Sidebar />
           <div className="flex--cont--def app-grid--container">
@@ -29,4 +41,4 @@ class App extends Component {
   }
 }
 
-export default App;
+export default translate("translations")(App);
